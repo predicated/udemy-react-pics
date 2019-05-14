683b5b38beca785de29fcc5a9259c1ea29baa388
Michael N. Rubinstein <michael@polyblog.com>
2019-05-14 16:54:12 -0700

    Fin Lesson 90: queried an API using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log("You submitted: " + term);
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 5966ec79bf5f80c9cd33b79255b73315e5e40e9e83e682395d4a53ba32aab656'
+            }
+        });
     }
 
     render() {
