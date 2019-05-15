e804b74b53230e0e2cc4d74bfd70311fd87c174e
Michael N. Rubinstein <michael@polyblog.com>
2019-05-14 17:34:00 -0700

    Fin Lesson 93: save fetch results in state

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,25 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos',{
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 5966ec79bf5f80c9cd33b79255b73315e5e40e9e83e682395d4a53ba32aab656'
             }
         });
+
+        //console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onHyloSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
